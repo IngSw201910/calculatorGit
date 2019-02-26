baf64c0a9a8c11ff1a6feb4aa42b27e20eb9ba68
Andres Parra <ca.parraa@javeriana.edu.co>
2019-02-25 22:19:46 -0500

    Initial Commit

---
+++ Calculator/src/co/edu/javeriana/gitsample/Authors.java
@@ -0,0 +1,6 @@
+package co.edu.javeriana.gitsample;
+
+public class Authors {
+	public static final String CAPA = "Carlos Parra";
+
+}
---
+++ Calculator/src/co/edu/javeriana/gitsample/Main.java
@@ -0,0 +1,11 @@
+package co.edu.javeriana.gitsample;
+
+public class Main {
+	
+	public static void main(String args[]){
+		//Create calculator implementation
+		//Calculator c = new Calculator()
+		//c.substract (100.5, 200)
+	}
+
+}
