a91601c30a8879b10b13dd7dd90e585103e09404
Nicolas Duarte <nicolas-duarte@javeriana.edu.co>
2019-02-28 09:33:28 -0500

    Nicolas implementacion

--- Calculator/src/co/edu/javeriana/gitsample/Main.java
+++ Calculator/src/co/edu/javeriana/gitsample/Main.java
@@ -7,6 +7,8 @@ public class Main {
 		//Create calculator implementation
 		ICalculator d = new CalculadoraDBS();
 		System.out.println(d.add(4, 5));
+		ICalculator n = new CalculatorNDO();
+		System.out.println(n.divide(899, 3));
 		
 		//c.substract (100.5, 200);
 		//Other operations
