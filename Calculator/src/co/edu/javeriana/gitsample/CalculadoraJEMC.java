f540ef368f92ecac0b219b9b7cb398faf0cc70aa
jonathan Molina <molina_jonathan@javeriana.edu.co>
2019-02-28 09:18:43 -0500

    commit inicial

---
+++ Calculator/src/co/edu/javeriana/gitsample/CalculadoraJEMC.java
@@ -0,0 +1,21 @@
+package co.edu.javeriana.gitsample;
+
+public class CalculadoraJEMC {
+
+	public double sumar(double a, double b){
+		return a+b;
+	}
+	public double restar(double a, double b){
+		return a-b;
+	}
+	public double multiplicar(double a, double b){
+		return a*b;
+	}
+	public double dividir(double a, double b){
+		return a/b;
+	}
+	public String about()
+	{
+		return "Calculator implemented by JEMC";
+	}
+}
